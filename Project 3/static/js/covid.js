cf38ee84b77147255f0ae4398fde996ef06f09fd
brittanynicole7 <bw.5552313@gmail.com>
2023-04-13 18:44:49 -0500

    Updated code

--- Project 3/static/js/covid.js
+++ Project 3/static/js/covid.js
@@ -3,24 +3,28 @@ function menu() {
     // Use D3 to select the dropdown menu
     let dropdownMenu = d3.select("#selDataset");
     // Assign the value of the dropdown menu option to a variable
-    d3.json("../../data/county_covid.json").then((data) => {
-    console.log(data)
-    let state = data.state
+    d3.json("./data/county_covid.json").then((data) => {
+    console.log(data);
+    
+    let state_list = [];
+    let state = data.state;
+    
+    console.log(state);
+        
         for (let i = 0; i < state.length; i++) 
         {
-            let state_list = [];
             let state_name = data.state[i];
-                if (state_name !== state_list) {
+                if (!state_list.includes(state_name)) {
             dropdownMenu
             .append('option')
-            .text(state[index])
-            .property('value', state[index])
+            .text(state[i])
+            .property('value', state[i])
             }
 
         };
 
 
-let firstSample = state[0]
+//let firstSample = state[0]
             /* buildBarchart(firstSample);
             buildMetaData(firstSample);
             bubbleChart(firstSample); */
