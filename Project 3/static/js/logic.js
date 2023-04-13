6d40fcf0abb5739cd9acd053f10ac01d8110d7b3
brittanynicole7 <bw.5552313@gmail.com>
2023-04-12 19:24:06 -0500

    Updates to code

--- Project 3/static/js/logic.js
+++ Project 3/static/js/logic.js
@@ -9,12 +9,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-let geoData = "https://api.covidactnow.org/v2/states.json?=dd72a0e3df1b4335aa98385505041054"
-
-let geoJson;
+let link = "../js/state_data.json"
 
 // Getting our GeoJSON data
-d3.json("https://api.covidactnow.org/v2/states.json?=dd72a0e3df1b4335aa98385505041054").then(function(data){
-console.log(data)
-}
-);
+d3.json(link).then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data
+  L.geoJson(data).addTo(myMap);
+}); 
+ 
+
+
