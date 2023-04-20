d3ea1e11fe47e31a924263dc2871af7721e53002
Phil-Mart <cooldrumsmusic@gmail.com>
2023-04-20 11:10:44 -0500

    11:10

--- Project 3/static/js/covid2.js
+++ Project 3/static/js/covid2.js
@@ -12,36 +12,36 @@ function menu() {
     let county_list = [];
     
         // Only push unique state values
-        for (const [key, value] of Object.entries(state)) {
+        for (const [key, value] of Object.entries(state)) 
             if (!state_list.includes(value))
             {state_list.push(value)};
-        };
+        
         
         // Loop through the list and append new states
-        for (let i = 0; i < state_list.length; i++) 
-         {
+        for (let i = 0; i < state_list .length; i++) 
+            {
             dropdownMenu
             .append('option')
-            .text(state_list[i])
-            .property('value', state_list[i]);
-            console.log(state_list[0]);
+            .text(state[i])
+            .property('value', state[i]);
             };
+        // console.log(state_list[0]);
 
 
     // Selecting the first state AK
-    let firstSample = county(county_list,state_list[0]);
-    console.log(county_list);
-    bubbleChart(county_list);
+    let firstSample = state[0];
+    console.log(firstSample);
+    bubbleChart(firstSample);
     });
 } 
 
 // Create a function for the dropdown menu to change
-function optionChanged(state_list){
+function optionChanged(state){
     county_list = [];
-    console.log(state_list);
-    let county_object = county(county_list,state_list);
-    console.log(county_list); 
-    bubbleChart(county_list);
+    console.log(state);
+    let county_object = county(state);
+    console.log(state); 
+    bubbleChart(state);
 }
 
 // Create a function to filter the county data by state
